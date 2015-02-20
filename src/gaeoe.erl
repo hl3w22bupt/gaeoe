3b044400e7bc189dcf00dca90e1ad4978e8c7efe
Liang Hu <lianghu@LiangdeMac-mini.local>
2015-02-20 22:06:42 +0800

    add gaeoe skeleton

---
+++ src/gaeoe.erl
@@ -0,0 +1,4 @@
+-module(gaeoe).
+-export([start/0]).
+
+start() -> io:format("Hello World~n").
---
+++ src/gaeoe_app.erl
@@ -0,0 +1,16 @@
+-module(gaeoe_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    gaeoe_sup:start_link().
+
+stop(_State) ->
+    ok.
